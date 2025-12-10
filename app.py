e626c737bbad9184e90f680cae97a34a9deb128c
nongtutien-beep <nongtutien@gmail.com>
2025-12-10 11:13:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nông Tư Tiến - Trường PTDTBT Tiểu học Bạch Đích - ĐT: 0848513222</div>", unsafe_allow_html=True)
